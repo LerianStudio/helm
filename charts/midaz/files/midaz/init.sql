b092a6798ca03333b45e7d20e526496bd6a10a69
gandalf-at-lerian <gandalf@lerian.studio>
2026-08-07 10:38:22 -0300

    fix(midaz): honor datastore broker port
    Requested-by: @guimoreirar

--- charts/midaz/files/midaz/init.sql
+++ charts/midaz/files/midaz/init.sql
@@ -7,8 +7,11 @@
 SELECT pg_create_physical_replication_slot('replication_slot');
 SELECT * FROM pg_create_logical_replication_slot('logical_slot', 'pgoutput');
 
-CREATE DATABASE {{ include "lerian-common.datastore.value" (dict "context" . "dedicated" .Values.ledger.datastores "configmap" (.Values.ledger.configmap | default dict) "type" "postgresOnboarding" "field" "name" "nativeKey" "DB_ONBOARDING_NAME" "default" "onboarding") }};
-CREATE DATABASE {{ include "lerian-common.datastore.value" (dict "context" . "dedicated" .Values.ledger.datastores "configmap" (.Values.ledger.configmap | default dict) "type" "postgresTransaction" "field" "name" "nativeKey" "DB_TRANSACTION_NAME" "default" "transaction") }};
+{{- $onboardingName := include "lerian-common.datastore.value" (dict "context" . "dedicated" .Values.ledger.datastores "configmap" (.Values.ledger.configmap | default dict) "type" "postgresOnboarding" "field" "name" "nativeKey" "DB_ONBOARDING_NAME" "default" "onboarding") }}
+{{- $transactionName := include "lerian-common.datastore.value" (dict "context" . "dedicated" .Values.ledger.datastores "configmap" (.Values.ledger.configmap | default dict) "type" "postgresTransaction" "field" "name" "nativeKey" "DB_TRANSACTION_NAME" "default" "transaction") }}
+{{- $tracerName := include "lerian-common.datastore.value" (dict "context" . "dedicated" .Values.tracer.datastores "configmap" (.Values.tracer.configmap | default dict) "type" "postgres" "field" "name" "nativeKey" "DB_NAME" "default" "tracer") }}
+CREATE DATABASE "{{ replace "\"" "\"\"" $onboardingName }}";
+CREATE DATABASE "{{ replace "\"" "\"\"" $transactionName }}";
 
 -- Tracer shares this cluster and the `midaz` role that owns the databases above
 -- (its Deployment reads the same subchart Secret). Created unconditionally, and
@@ -17,4 +20,4 @@ CREATE DATABASE {{ include "lerian-common.datastore.value" (dict "context" . "de
 -- An unused empty database costs nothing. The name tracks
 -- tracer.configmap.DB_NAME so the bundled cluster and the external bootstrap
 -- Job create the same database the tracer service and its migration Job read.
-CREATE DATABASE {{ include "lerian-common.datastore.value" (dict "context" . "dedicated" .Values.tracer.datastores "configmap" (.Values.tracer.configmap | default dict) "type" "postgres" "field" "name" "nativeKey" "DB_NAME" "default" "tracer") }};
+CREATE DATABASE "{{ replace "\"" "\"\"" $tracerName }}";
